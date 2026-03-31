3a94e6781b9fb452db01729da611c4cc03f8d497
ebarrado <erika_barrado@hotmail.com>
2026-03-31 13:24:23 -0300

    Atualizacao

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ console.log(`🌐 PORT: ${process.env.PORT || 3000}`);
 
 // ==================== APP ====================
 const app = express();
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const frontendPath = path.join(__dirname, '../frontend/dist');
 
 // ==================== SERVIÇOS ====================
